6ef41dc5e12ac3167e968e4d7f3cc8de7bbabc5c
rytis <rytisvenslovas@gmail.com>
2021-03-12 12:37:12 +0000

    8Hour

--- admin-dashboard-front/src/components/NewQueries.js
+++ admin-dashboard-front/src/components/NewQueries.js
@@ -7,17 +7,27 @@ const NewQueries = ({allNewQueries}) => {
     let history = useHistory()
 
 
-    const buttonGeneralQueries = querieType === "general queries"  ?  "button is-primary is-inverted" : "button is-primary"
-    console.log(queries)
+    const buttonGeneralQueries = querieType === "generalQueries"  ?  "button is-success is-inverted" : "button is-success"
+    const buttonCompanyMattersQueries = querieType === "companyMattersQueries"  ?  "button is-success is-inverted" : "button is-success"
+    const buttonNewCompanyEstablishmentQueries = querieType === "newCompanyEstablishmentQueries"  ?  "button is-success is-inverted" : "button is-success"
+    const buttonSelfEmployedQueries = querieType === "selfEmployedQueries"  ?  "button is-success is-inverted" : "button is-success"
 
+    const pressButton = (buttonType) => {
+
+        if(querieType === buttonType){
+            setQuerieType("")
+            setQueries([])
+        }else{setQuerieType(buttonType)}
+
+    }
 
 return (
     <div id="all-queries-div">
         <div id="all-queries-buttons">
-            <button className={buttonGeneralQueries} onClick={()=>{setQueries(allNewQueries.generalQueries)}}>General queries ({allNewQueries.generalQueries.length})</button>
-            <button className="button is-primary" onClick={()=>{setQueries(allNewQueries.companyMattersQueries)}}>Company matters queries ({allNewQueries.companyMattersQueries.length})</button>
-            <button className="button is-primary" onClick={()=>{setQueries(allNewQueries.newCompanyEstablishmentQueries)}}>New company establish queries ({allNewQueries.newCompanyEstablishmentQueries.length})</button>
-            <button className="button is-primary" onClick={()=>{setQueries(allNewQueries.selfEmployedQueries)}}>Self employed queries ({allNewQueries.selfEmployedQueries.length})</button>
+            <button className={buttonGeneralQueries} onClick={()=>{setQueries(allNewQueries.generalQueries); pressButton('generalQueries')}}>General queries ({allNewQueries.generalQueries.length})</button>
+            <button className={buttonCompanyMattersQueries} onClick={()=>{setQueries(allNewQueries.companyMattersQueries); pressButton('companyMattersQueries')}}>Company matters queries ({allNewQueries.companyMattersQueries.length})</button>
+            <button className={buttonNewCompanyEstablishmentQueries} onClick={()=>{setQueries(allNewQueries.newCompanyEstablishmentQueries); pressButton('newCompanyEstablishmentQueries')}}>New company establish queries ({allNewQueries.newCompanyEstablishmentQueries.length}) </button>
+            <button className={buttonSelfEmployedQueries} onClick={()=>{setQueries(allNewQueries.selfEmployedQueries);pressButton('selfEmployedQueries')}}>Self employed queries ({allNewQueries.selfEmployedQueries.length})</button>
         </div>
         <div>
             {queries.map((querie)=>(
--- admin-dashboard-front/src/components/Querie.js
+++ admin-dashboard-front/src/components/Querie.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams, useHistory } from "react-router-dom";
+import { Fab } from '@material-ui/core';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import axios from 'axios'
 
 
@@ -14,12 +16,14 @@ import ClientToDatabaseForm from './forms/clientToDatabase'
 
 const Querie = ({allNewQueries, clients}) => {
     const [visible, setVisible] = useState(false)
-    const [querie, setQuerie] = useState([])
+    const [querie, setQuerie] = useState(null)
     const [thumbnail, setThumbnail] = useState([])
     const [loading, setLoading] = useState("");
     const [serverResponse, setServerResponse] = useState("")
     const type = "Save to the database"
     let history = useHistory()
+
+    let omitedQuerie 
 
     let { id } = useParams();
 
@@ -30,7 +34,12 @@ const Querie = ({allNewQueries, clients}) => {
         }})})
    },[])
 
-   
+   if(querie){
+    const { __v, _id, type, ...rest} = querie
+    omitedQuerie = rest
+    }
+
+    console.log(querie)
 
    const saveQuerieToDatabase = (values) => {
     setLoading("loading")
@@ -58,79 +67,71 @@ const Querie = ({allNewQueries, clients}) => {
     }
 
 return (
-    <div className="columns">
-        <div className="column is-centered" >
-                        { loading === "loading" ? 
-                                <div className="loader-wrapper" style={{"height":"100%", "width":"250px", "display":"flex","justifyContent":"center","alignItems":"center"}}>
-                                    <div className="loader is-loading" style={{"height":"100px", "width":"100px"}}></div>
-                                </div> 
-                                :
-                                loading === "successful" ?
-                                                        <div className="notification is-success" style={{"width":"250px"}}>
-                                                            <button className="delete" onClick={()=>{setLoading(false)}}></button>
-                                                            <p className="subtitle">{serverResponse}</p>
-                                                        </div>
-                                :
-                                loading === "error" ?
-                                                    <div className="notification is-danger" style={{"width":"250px"}}>
-                                                        <button className="delete" onClick={()=>{setLoading(false)}}></button>
-                                                        <p className="subtitle">{serverResponse}</p>
-                                                    </div>
-                                :
-                                null
-                        }
-            </div>
-        <div className="columns" style={{"border": "solid"}}>
-            <p><strong>{querie.name}</strong> | {querie.date} | <strong>{querie.type}</strong> <button onClick={()=>{clientDatabaseServices.deleteQuerie(querie.type, querie._id).then((res)=>{console.log(res)})}}>delete</button></p>
-            <div id="to-dropbox-folder">
-                <a className="button" as="button" target="_blank" href={`https://www.dropbox.com/home/Clients/${id}`} id="client-component-button">
-                    <img alt="dropbox" src="https://cfl.dropboxstatic.com/static/images/logo_catalog/dropbox_logo_glyph_m1.svg" style={{"maxHeight":"34px", "paddingLeft": "10px"}}></img>
-                    <img alt="dropbox" src="https://cfl.dropboxstatic.com/static/images/logo_catalog/wordmark--dropbox_m1.svg" style={{"maxHeight":"34px", "paddingLeft": "10px"}}></img>
-                </a>
-            </div>
+    <div>
+        {querie ? 
             <div>
-                <div  style={{"backgroundColor":"black", "width":"600px", "opacity":"0.9"}}>
-                    {Object.keys(querie).map((key)=>{  // sumepina visa query
-                            if(Array.isArray(querie[key])){
-                                return (
-                                    <div className="columns">
-                                        <h1>Shareholders</h1>
-                                        {querie[key].map((element)=>{
-                                            return(
-                                                <div className="column">
-                                                    {Object.keys(element).map((keyInArray)=>{
-                                                        return(
-                                                            <div>
-                                                                <p><strong>{keyInArray.split(/(?=[A-Z])/).join(" ")}</strong> : {element[keyInArray]}</p>
-                                                            </div>
-                                                        )
-                                                    })}
-                                                </div>
-                                            )
-                                        })}
-                                    </div>
-                                )
-                            }else if(key === "file"){
-                                return (
-                                    <div className="column">
-                                        <p><strong>{key}</strong> : <a href={`https://www.dropbox.com/home/ToBeConfirmed/${id}`} target="_blank">{querie[key]}</a></p>
-                                    </div>
-                                )
-                            }
-                            else {
-                                return(
-                                    <div> 
-                                        <p><strong>{key.split(/(?=[A-Z])/).join(" ")}</strong> : {querie[key]}</p>
-                                    </div>
-                                )
-                            }
-                        })
-                    }
+            <div id="client-info">
+                        <h1 className="title">{querie.name} <strong className="subtitle is-4" style={{"color":"black"}}> {querie.date.split("GMT")[0]} </strong> {querie.type} </h1>
+                        <div id="client-info-buttons">
+                            <a role="button" target="_blank" href={`https://www.dropbox.com/home/ToBeConfirmed/${id}`} id="client-component-button" >
+                                <Fab>
+                                    <img alt="dropbox" src="https://cfl.dropboxstatic.com/static/images/logo_catalog/dropbox_logo_glyph_m1.svg" style={{"maxHeight":"34px"}}></img>
+                                </Fab>
+                            </a>
+                        </div>
+                        <div style={{"padding": "10px"}}></div>
+                        <div id="client-info-buttons">
+                            <a role="button" onClick={()=>{console.log("SWX")}}>
+                                <Fab>
+                                    <DeleteForeverIcon style={{"color": "#ff000080"}} fontSize="large"/>
+                                </Fab>
+                            </a>
+                        </div>
                 </div>
-                <div className="column">
-                    {querie.length !== 0 ? <ClientToDatabaseForm clients={clients} type={type} querie={querie} saveQuerieToDatabase={saveQuerieToDatabase}/> : <p>Querie not found </p>}
+                
+                <div>
+                    <div id="querie-div">
+                        {Object.keys(omitedQuerie).map((key)=>{  // sumepina visa query
+                                if(Array.isArray(querie[key])){
+                                    return (
+                                        <div id="shareholders" className="columns">
+                                            {querie[key].map((element)=>{
+                                                return(
+                                                    <div >
+                                                        {Object.keys(element).map((keyInArray)=>{
+                                                            return(
+                                                                <div style={{"backgroundColor":"grey"}}>
+                                                                    <p><strong>{keyInArray.split(/(?=[A-Z])/).join(" ")}</strong> : {element[keyInArray]}</p>
+                                                                </div>
+                                                            )
+                                                        })}
+                                                    </div>
+                                                )
+                                            })}
+                                        </div>
+                                    )
+                                }else if(key === "file"){
+                                    return (
+                                        <div className="columns">
+                                            <p><strong>{key}</strong> : <a href={`https://www.dropbox.com/home/ToBeConfirmed/${id}`} target="_blank">{querie[key]}</a></p>
+                                        </div>
+                                    )
+                                }
+                                else {
+                                    return(
+                                        <div className="columns"> 
+                                            <p><strong>{key.split(/(?=[A-Z])/).join(" ")}</strong> : {querie[key]}</p>
+                                        </div>
+                                    )
+                                }
+                            })
+                        }
+                    </div>
                 </div>
-                 </div>
+            </div>
+        : null }
+        <div>
+            {querie ? <ClientToDatabaseForm clients={clients} type={type} querie={querie} saveQuerieToDatabase={saveQuerieToDatabase}/> : <p>Querie not found </p>}
         </div>
     </div>
 )
